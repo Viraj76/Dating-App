217a8527b44e0325baf21310d959567d115c48cb
Viraj76 <virajg.7680@gmail.com>
2023-03-30 19:54:03 +0530

    Showing user ( just for now)

--- app/src/main/java/com/example/datingapp/adapter/DatingAdapter.kt
+++ app/src/main/java/com/example/datingapp/adapter/DatingAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.datingapp.adapter
 
-import android.service.autofill.UserData
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,8 +9,11 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.example.datingapp.databinding.ItemKolodaBinding
 import com.example.datingapp.model.UserModel
+import com.example.datingapp.ui.activity.MainActivity
+import com.example.datingapp.ui.activity.MessageActivity
+import com.example.datingapp.ui.fragments.DatingFragment
 
-class DatingAdapter:RecyclerView.Adapter<DatingAdapter.UserViewHolder>() {
+class DatingAdapter(val context: Context):RecyclerView.Adapter<DatingAdapter.UserViewHolder>() {
 
     private  var userDetail = ArrayList<UserModel>()
     fun setUserDetail(userDetail: ArrayList<UserModel>){
@@ -25,11 +29,18 @@ class DatingAdapter:RecyclerView.Adapter<DatingAdapter.UserViewHolder>() {
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
        val details = userDetail[position]
+
         holder.binding.apply {
             Glide.with(holder.itemView).load(details.image).into(userImage)
             userName.text = details.name
             userEmail.text = details.email
+            chatCard.setOnClickListener {
+                val intent = Intent(context,MessageActivity::class.java)
+                intent.putExtra("userId",details.number)
+                context.startActivity(intent)
+            }
         }
+
     }
 
     override fun getItemCount(): Int {
