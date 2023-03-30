8de03950bca65e7ae8f493e971f242f1e975941a
Viraj76 <virajg.7680@gmail.com>
2023-03-30 19:52:20 +0530

    Adapter for users on chat frag

--- app/src/main/java/com/example/datingapp/adapter/ChatFragmentAdapter.kt
+++ app/src/main/java/com/example/datingapp/adapter/ChatFragmentAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.datingapp.adapter
 
-class ChatFragmentAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.datingapp.databinding.ChatItemViewBinding
+import com.example.datingapp.model.UserModel
+
+class ChatFragmentAdapter:RecyclerView.Adapter<ChatFragmentAdapter.ChatViewHolder>() {
+
+   private var userList = ArrayList<UserModel>()
+
+    fun setUserList(userList:ArrayList<UserModel>){
+        this.userList = userList
+        notifyDataSetChanged()
+    }
+
+    class ChatViewHolder(val binding:ChatItemViewBinding):ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+        return ChatViewHolder(ChatItemViewBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+       val userData  = userList[position]
+        holder.binding.apply {
+            Glide.with(holder.itemView).load(userData.image).into(userImage)
+            userName.text = userData.name
+        }
+    }
+
+    override fun getItemCount(): Int {
+       return userList.size
+    }
+
+
 }
