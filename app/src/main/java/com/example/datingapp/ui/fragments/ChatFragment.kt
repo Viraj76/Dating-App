cdb1d2ddc0ebd72bc70de9ae7afbb8b09d9f6018
Viraj76 <virajg.7680@gmail.com>
2023-03-30 19:53:17 +0530

    Showing user ( just for now)

--- app/src/main/java/com/example/datingapp/ui/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/datingapp/ui/fragments/ChatFragment.kt
@@ -1,21 +1,45 @@
-package com.example.datingapp.ui
+package com.example.datingapp.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentContainer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.datingapp.R
+import com.example.datingapp.adapter.ChatFragmentAdapter
+import com.example.datingapp.databinding.FragmentChatragmentBinding
+import com.example.datingapp.ui.fragments.DatingFragment.Companion.userDetail
 
 
 class ChatFragment : Fragment() {
 
+    private lateinit var binding:FragmentChatragmentBinding
+    private lateinit var chatFragmentAdapter: ChatFragmentAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chatragment, container, false)
+    ): View {
+        binding = FragmentChatragmentBinding.inflate(layoutInflater)
+
+        prepareRvForChatFragmentAdapter()
+        showingAllUsers()
+
+        return binding.root
+    }
+
+    private fun showingAllUsers() {
+        chatFragmentAdapter.setUserList(userDetail!!)
+    }
+
+    private fun prepareRvForChatFragmentAdapter() {
+        chatFragmentAdapter = ChatFragmentAdapter()
+        binding.userChatRv.apply {
+            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
+            adapter = chatFragmentAdapter
+        }
     }
 
 
