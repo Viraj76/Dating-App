75b5c07dd747cf69b86c0b8d61e9551798afa74e
Viraj76 <virajg.7680@gmail.com>
2023-03-31 19:25:52 +0530

    Model for message

--- app/src/main/java/com/example/datingapp/model/Messages.kt
+++ app/src/main/java/com/example/datingapp/model/Messages.kt
@@ -1,4 +1,8 @@
 package com.example.datingapp.model
 
-class Messages {
-}
+data class Messages(
+    val message : String ?=null,
+    val senderId : String ?= null,
+    val currentTime : String ?= null,
+    val currentDate : String ?= null
+)
