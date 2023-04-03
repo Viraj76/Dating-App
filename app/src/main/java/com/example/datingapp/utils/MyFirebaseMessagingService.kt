ffbf228f649919202d78d9b0a788d56c822305b7
Viraj76 <virajg.7680@gmail.com>
2023-04-04 01:20:54 +0530

    Able to receive the notification from postman in the mobile

--- app/src/main/java/com/example/datingapp/utils/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/datingapp/utils/MyFirebaseMessagingService.kt
@@ -2,23 +2,55 @@ package com.example.datingapp.utils
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.datingapp.R
+import com.example.datingapp.ui.activity.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
 
+    private val channelId = "viraj"
 
-//    @RequiresApi(Build.VERSION_CODES.O)
-//    private fun createNotificationChannel(manager : NotificationManager) {
-//        val channel = NotificationChannel(channelId, name: "papayacoderschat",
-//        NotificationManager.IMPORTANCE_HIGH)
-//
-//        channel.description = "New Chat"
-//        channel.enableLights(lights: true)
-//        manager.createNotificationChannel(channel)
-//
-//    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        val intent = Intent(this,MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+        createNotificationChannel(manager as NotificationManager)
+
+        val intent1 = PendingIntent.getActivities(this,0, arrayOf(intent),PendingIntent.FLAG_IMMUTABLE)
+
+        val notification = NotificationCompat.Builder(this,channelId)
+            .setContentTitle(message.data["title"])
+            .setContentTitle(message.data["message"])
+            .setSmallIcon(R.drawable.notifications)
+            .setAutoCancel(true)
+            .setContentIntent(intent1)
+            .build()
+
+        manager.notify(Random.nextInt(),notification)
+
+    }
+
+
+
+    private fun createNotificationChannel(manager : NotificationManager) {
+        val channel = NotificationChannel(channelId, "virajchat",
+        NotificationManager.IMPORTANCE_HIGH)
+
+        channel.description = "New Chat"
+        channel.enableLights( true)
+        manager.createNotificationChannel(channel)
+
+    }
 
 }
